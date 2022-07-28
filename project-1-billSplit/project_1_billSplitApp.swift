c3eb754226993dc544b75ee7bde3f0ae817fa182
adarsh <adarsh0ranjan@gmail.com>
2022-07-28 13:21:18 +0530

    Initial Commit

---
+++ project-1-billSplit/project_1_billSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_1_billSplitApp.swift
+//  project-1-billSplit
+//
+//  Created by Roro Solutions on 28/07/22.
+//
+
+import SwiftUI
+
+@main
+struct project_1_billSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
